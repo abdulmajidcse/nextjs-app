8f3c11ad1b956f929b954f2ea4bf9ebfe4e90118
Abdul Majid <abdulmajidcse@gmail.com>
2021-09-04 13:04:37 +0600

    added bootstrap icon and setup light and dark theme

--- components/Footer.js
+++ components/Footer.js
@@ -1,10 +1,14 @@
+import { ThemeContext } from '../contexts/themeContext'
+import { useContext } from 'react'
 import { Container } from "react-bootstrap"
 
 export default function Footer() {
+    const { theme } = useContext(ThemeContext)
+
     return (
-        <div className="p-3 bg-light">
+        <div className={`p-3 ${theme === 'light' ? 'bg-light' : 'bg-dark'}`}>
             <Container>
-                <div className="text-center">
+                <div className={`text-center ${theme === 'light' ? 'text-dark' : 'text-light'}`}>
                     <p>2021 &copy; Developed By <a href="https://facebook.com/abdulmajidcse">Abdul Majid</a></p>
                 </div>
             </Container>
--- components/Header.js
+++ components/Header.js
@@ -1,23 +1,24 @@
 import { Container, Navbar, Nav } from 'react-bootstrap'
 import Link from 'next/link'
 import { ThemeContext } from '../contexts/themeContext'
-import { useContext } from 'react';
+import { useContext } from 'react'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 export default function Header() {
     const { theme, changeTheme } = useContext(ThemeContext)
 
     return (
-        <div className="bg-light">
+        <div className={theme === 'light' ? 'bg-light' : 'bg-dark'}>
             <Container>
-                <Navbar bg="light" expand="lg">
-                <Link href="/" title={process.env.APP_NAME}><a className="text-decoration-none"><Navbar.Brand>{process.env.APP_NAME}</Navbar.Brand></a></Link>
+                <Navbar expand="lg">
+                <Link href="/" title={process.env.APP_NAME}><a className="text-decoration-none"><Navbar.Brand className={theme === 'light' ? 'text-dark' : 'text-light'}>{process.env.APP_NAME}</Navbar.Brand></a></Link>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ms-auto">
-                            <Link href="/"><a className="nav-link">Home</a></Link>
-                            <Link href="/todos"><a className="nav-link">Todo List</a></Link>
-                            <span style={{cursor: 'pointer'}} className="nav-link" onClick={changeTheme}>
-                                {theme === 'light' ? 'Dark Theme' : 'Light Theme'}
+                            <Link href="/"><a className={`nav-link ${theme === 'light' ? 'text-dark' : 'text-light'}`}>Home</a></Link>
+                            <Link href="/todos"><a className={`nav-link ${theme === 'light' ? 'text-dark' : 'text-light'}`}>Todo List</a></Link>
+                            <span style={{cursor: 'pointer'}} className={`nav-link ${theme === 'light' ? 'text-dark' : 'text-light'}`} onClick={changeTheme}>
+                                {theme === 'light' ? <i className="bi bi-moon-fill"></i> : <i className="bi bi-moon"></i>}
                             </span>
                         </Nav>
                     </Navbar.Collapse>
