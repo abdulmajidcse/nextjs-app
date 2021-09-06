40bb99e886c28aee48915f0c7c18e68a2a203b7c
Abdul Majid <abdulmajidcse@gmail.com>
2021-09-06 12:38:46 +0600

    update syntax style

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  reactStrictMode: true,
-  env: {
-    APP_NAME: 'NextJS App',
-    API_URL: 'https://talktoapi.abdulmajid.me/api',
-  }
-}
+    reactStrictMode: true,
+    env: {
+        APP_NAME: 'NextJS App',
+        API_URL: 'https://talktoapi.abdulmajid.me/api',
+    },
+};
